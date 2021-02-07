1fb471f8fb99f829b5dfa577017c3b729a11b8c7
Amrita <amritaravishankar00@gmail.com>
2021-02-07 11:59:00 +0800

    Implement shared expense pop up

--- front-end/src/app/pages/shared-expense/components/input-expense/input-expense.component.ts
+++ front-end/src/app/pages/shared-expense/components/input-expense/input-expense.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  name: string;
+  category: string;
+  amount: string;
+  friend: string;
+}
 
 @Component({
   selector: 'app-input-expense',
   templateUrl: './input-expense.component.html',
   styleUrls: ['./input-expense.component.scss']
 })
+
 export class InputExpenseComponent implements OnInit {
 
-  constructor() { }
+  categories = ['Food', 'Travel', 'Shopping', 'Others'];
+  category = 'Others';
+  friend = "";
 
-  ngOnInit(): void {
+  friends = ['Anusha', 'Mehul']
+
+  @Input() updateExpense: boolean;
+  
+  constructor(
+    public dialogRef: MatDialogRef<InputExpenseComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
+  ngOnInit(): void {
+  }  
+
+  addExpense():void {
+    this.data.category = this.category;
+    this.data.friend = this.friend;
+    console.log(this.data);
+  }
 }
+
