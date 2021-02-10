05bb20d9d27fed4c558e8b9c7c44c4d16970af6d
amritaravishankar <amritaravishankar00@gmail.com>
2021-02-10 11:47:46 +0800

    Implement shared input expense pop up

--- front-end/src/app/pages/shared-expense/components/split-by/split-by.component.ts
+++ front-end/src/app/pages/shared-expense/components/split-by/split-by.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface SplitMethodData {
+  split_by_method: string;
+}
 
 @Component({
   selector: 'app-split-by',
   templateUrl: './split-by.component.html',
   styleUrls: ['./split-by.component.scss']
 })
+
 export class SplitByComponent implements OnInit {
 
   methods = ['Equally', 'Shares', 'Amount'];
-  constructor() { }
+  split_by_method = "";
+
+  constructor(public dialogRef: MatDialogRef<SplitByComponent>, public dialog: MatDialog,  @Inject(MAT_DIALOG_DATA) public data: SplitMethodData) { }
 
   ngOnInit(): void {
   }
+
+  onNoClickSplit(): void {
+    this.dialogRef.close();
+  }
+
+  onConfirm(): void {
+    this.data.split_by_method = this.split_by_method;
+    console.log(this.data);
+  }
+
 
 }
